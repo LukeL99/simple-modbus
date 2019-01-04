fe6189d94e0cc8bbe9332bd449da1bd3cfe9a0d3
Luke Libraro <luke@rw.co>
2019-01-04 11:49:10 -0600

    style(tcp-server.test.ts): remove unused import

--- src/tcp/tcp-server.test.ts
+++ src/tcp/tcp-server.test.ts
@@ -6,16 +6,16 @@ jest.mock('net')
 import { ModbusTcpServer, ModbusTcpServerOptions } from './modbus-tcp-server'
 import {
   ForceMultipleCoilsCommand,
-  ForceSingleCoilCommand, PresetMultipleRegistersCommand,
+  ForceSingleCoilCommand,
+  PresetMultipleRegistersCommand,
   PresetSingleRegisterCommand,
   ReadCoilStatusCommand,
-  ReadHoldingRegistersCommand, ReadInputRegistersCommand,
+  ReadHoldingRegistersCommand,
+  ReadInputRegistersCommand,
   ReadInputStatusCommand
 } from '../modbus-commands'
-import { TypedEvent } from '../util/typed-event'
 
 describe('Server tests', () => {
-
   beforeEach(() => {
     jest.clearAllMocks()
     net.__reset()
@@ -33,7 +33,7 @@ describe('Server tests', () => {
     expect(net.__server.close.mock.calls.length).toBe(1)
   })
 
-  it('should emit a server error on TCP server error', (done) => {
+  it('should emit a server error on TCP server error', done => {
     const server = new ModbusTcpServer().listen(502)
 
     server.onServerError.on(e => {
@@ -61,11 +61,24 @@ describe('Server tests', () => {
     expect(server._commandFactory._options).toEqual(options)
   })
 
-  it('should use simple addressing when simpleAddressing is blank', (done) => {
-    const validRequest = Buffer.from([0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x11, 0x06, 0x00, 0x00, 0x00, 0x03])
+  it('should use simple addressing when simpleAddressing is blank', done => {
+    const validRequest = Buffer.from([
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x11,
+      0x06,
+      0x00,
+      0x00,
+      0x00,
+      0x03
+    ])
     const server = new ModbusTcpServer().listen(502)
 
-    server.onPresetSingleRegister.on((command) => {
+    server.onPresetSingleRegister.on(command => {
       expect(command).toBeInstanceOf(PresetSingleRegisterCommand)
       expect(command.registerAddress).toEqual(0)
       done()
@@ -74,11 +87,24 @@ describe('Server tests', () => {
     net.__socket.emit('data', validRequest)
   })
 
-  it('should use simple addressing when simpleAddressing is true', (done) => {
-    const validRequest = Buffer.from([0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x11, 0x06, 0x00, 0x00, 0x00, 0x03])
+  it('should use simple addressing when simpleAddressing is true', done => {
+    const validRequest = Buffer.from([
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x11,
+      0x06,
+      0x00,
+      0x00,
+      0x00,
+      0x03
+    ])
     const server = new ModbusTcpServer({ simpleAddressing: true }).listen(502)
 
-    server.onPresetSingleRegister.on((command) => {
+    server.onPresetSingleRegister.on(command => {
       expect(command).toBeInstanceOf(PresetSingleRegisterCommand)
       expect(command.registerAddress).toEqual(0)
       done()
@@ -87,11 +113,24 @@ describe('Server tests', () => {
     net.__socket.emit('data', validRequest)
   })
 
-  it('should use Modbus addressing when simpleAddressing is false', (done) => {
-    const validRequest = Buffer.from([0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x11, 0x06, 0x00, 0x00, 0x00, 0x03])
+  it('should use Modbus addressing when simpleAddressing is false', done => {
+    const validRequest = Buffer.from([
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x11,
+      0x06,
+      0x00,
+      0x00,
+      0x00,
+      0x03
+    ])
     const server = new ModbusTcpServer({ simpleAddressing: false }).listen(502)
 
-    server.onPresetSingleRegister.on((command) => {
+    server.onPresetSingleRegister.on(command => {
       expect(command).toBeInstanceOf(PresetSingleRegisterCommand)
       expect(command.registerAddress).toEqual(40001)
       done()
@@ -99,30 +138,90 @@ describe('Server tests', () => {
 
     net.__socket.emit('data', validRequest)
   })
-
 })
 
 describe('Server command tests', () => {
-
   beforeEach(() => {
     jest.clearAllMocks()
     net.__reset()
   })
 
-  it('should emit a ReadCoilStatusCommand and write a response', (done) => {
-    const validCommandBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x01, 0x01, 0x10, 0x00, 0x25]
+  it('should emit a ReadCoilStatusCommand and write a response', done => {
+    const validCommandBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x01,
+      0x01,
+      0x10,
+      0x00,
+      0x25
+    ]
 
-    const coilValues = [true, false, true, true, false, false, true, true,
-      true, true, false, true, false, true, true, false,
-      false, true, false, false, true, true, false, true,
-      false, true, true, true, false, false, false, false,
-      true, true, false, true, true]
+    const coilValues = [
+      true,
+      false,
+      true,
+      true,
+      false,
+      false,
+      true,
+      true,
+      true,
+      true,
+      false,
+      true,
+      false,
+      true,
+      true,
+      false,
+      false,
+      true,
+      false,
+      false,
+      true,
+      true,
+      false,
+      true,
+      false,
+      true,
+      true,
+      true,
+      false,
+      false,
+      false,
+      false,
+      true,
+      true,
+      false,
+      true,
+      true
+    ]
 
-    const validResponseBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x05, 0x01, 0x05, 0xCD, 0x6B, 0xB2, 0x0E, 0x1B]
+    const validResponseBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x08,
+      0x05,
+      0x01,
+      0x05,
+      0xcd,
+      0x6b,
+      0xb2,
+      0x0e,
+      0x1b
+    ]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onReadCoilStatus.on((command) => {
+    server.onReadCoilStatus.on(command => {
       expect(command).toBeInstanceOf(ReadCoilStatusCommand)
       net.__socket.on('write', (data: any) => {
         expect(data).toEqual(Buffer.from(validResponseBytes))
@@ -134,20 +233,65 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(validCommandBytes))
   })
 
-  it('should emit a ReadInputStatusCommand and write a response', (done) => {
-    const validCommandBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x02, 0x01, 0x10, 0x00, 0x16]
-
-    const inputStatuses = [
-      false, false, true, true, false, true, false, true,
-      true, true, false, true, true, false, true, true,
-      true, false, true, false, true, true
+  it('should emit a ReadInputStatusCommand and write a response', done => {
+    const validCommandBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x02,
+      0x01,
+      0x10,
+      0x00,
+      0x16
     ]
 
-    const validResponseBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x02, 0x03, 0xAC, 0xDB, 0x35]
+    const inputStatuses = [
+      false,
+      false,
+      true,
+      true,
+      false,
+      true,
+      false,
+      true,
+      true,
+      true,
+      false,
+      true,
+      true,
+      false,
+      true,
+      true,
+      true,
+      false,
+      true,
+      false,
+      true,
+      true
+    ]
+
+    const validResponseBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x02,
+      0x03,
+      0xac,
+      0xdb,
+      0x35
+    ]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onReadInputStatus.on((command) => {
+    server.onReadInputStatus.on(command => {
       expect(command).toBeInstanceOf(ReadInputStatusCommand)
       net.__socket.on('write', (data: any) => {
         expect(data).toEqual(Buffer.from(validResponseBytes))
@@ -159,17 +303,45 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(validCommandBytes))
   })
 
-  it('should emit a ReadHoldingRegistersCommand and write a response', (done) => {
+  it('should emit a ReadHoldingRegistersCommand and write a response', done => {
+    const validCommandBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x03,
+      0x01,
+      0x10,
+      0x00,
+      0x03
+    ]
 
-    const validCommandBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x03, 0x01, 0x10, 0x00, 0x03]
+    const registerValues = new Uint16Array([0xae41, 0x5652, 0x4340])
 
-    const registerValues = new Uint16Array([0xAE41, 0x5652, 0x4340])
-
-    const validResponseBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x09, 0x05, 0x03, 0x06, 0xAE, 0x41, 0x56, 0x52, 0x43, 0x40]
+    const validResponseBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x09,
+      0x05,
+      0x03,
+      0x06,
+      0xae,
+      0x41,
+      0x56,
+      0x52,
+      0x43,
+      0x40
+    ]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onReadHoldingRegisters.on((command) => {
+    server.onReadHoldingRegisters.on(command => {
       expect(command).toBeInstanceOf(ReadHoldingRegistersCommand)
       net.__socket.on('write', (data: any) => {
         expect(data).toEqual(Buffer.from(validResponseBytes))
@@ -181,17 +353,45 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(validCommandBytes))
   })
 
-  it('should emit a ReadInputRegistersCommand and write a response', (done) => {
+  it('should emit a ReadInputRegistersCommand and write a response', done => {
+    const validCommandBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x04,
+      0x01,
+      0x10,
+      0x00,
+      0x03
+    ]
 
-    const validCommandBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x04, 0x01, 0x10, 0x00, 0x03]
+    const registerValues = new Uint16Array([0xae41, 0x5652, 0x4340])
 
-    const registerValues = new Uint16Array([0xAE41, 0x5652, 0x4340])
-
-    const validResponseBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x09, 0x05, 0x04, 0x06, 0xAE, 0x41, 0x56, 0x52, 0x43, 0x40]
+    const validResponseBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x09,
+      0x05,
+      0x04,
+      0x06,
+      0xae,
+      0x41,
+      0x56,
+      0x52,
+      0x43,
+      0x40
+    ]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onReadInputRegisters.on((command) => {
+    server.onReadInputRegisters.on(command => {
       expect(command).toBeInstanceOf(ReadInputRegistersCommand)
       net.__socket.on('write', (data: any) => {
         expect(data).toEqual(Buffer.from(validResponseBytes))
@@ -203,12 +403,12 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(validCommandBytes))
   })
 
-  it('should emit a ForceSingleCoilCommand and write a response', (done) => {
-    const coilOnBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x05, 0x01, 0x10, 0xFF, 0x00]
+  it('should emit a ForceSingleCoilCommand and write a response', done => {
+    const coilOnBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x05, 0x01, 0x10, 0xff, 0x00]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onForceSingleCoil.on((command) => {
+    server.onForceSingleCoil.on(command => {
       expect(command).toBeInstanceOf(ForceSingleCoilCommand)
       net.__socket.on('write', (data: any) => {
         expect(data).toEqual(Buffer.from(coilOnBytes))
@@ -220,12 +420,12 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(coilOnBytes))
   })
 
-  it('should emit an error when invalid ForceSingleCoilCommand is sent', (done) => {
+  it('should emit an error when invalid ForceSingleCoilCommand is sent', done => {
     const coilFailBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x05, 0x01, 0x10, 0x11, 0x11]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onCommandError.on((e) => {
+    server.onCommandError.on(e => {
       expect(e).toBeInstanceOf(ModbusCommandError)
       expect(e.message).toEqual('FORCE_SINGLE_COIL - Invalid coil status received')
       expect(e.requestBytes).toEqual(Buffer.from(coilFailBytes))
@@ -235,12 +435,38 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(coilFailBytes))
   })
 
-  it('should emit a PresetSingleRegisterCommand and write a response', (done) => {
-    const validRequest = Buffer.from([0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x11, 0x06, 0x00, 0x00, 0x00, 0x03])
-    const validResponse = Buffer.from([0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x11, 0x06, 0x00, 0x00, 0x00, 0x03])
+  it('should emit a PresetSingleRegisterCommand and write a response', done => {
+    const validRequest = Buffer.from([
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x11,
+      0x06,
+      0x00,
+      0x00,
+      0x00,
+      0x03
+    ])
+    const validResponse = Buffer.from([
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x11,
+      0x06,
+      0x00,
+      0x00,
+      0x00,
+      0x03
+    ])
     const server = new ModbusTcpServer().listen(502)
 
-    server.onPresetSingleRegister.on((command) => {
+    server.onPresetSingleRegister.on(command => {
       expect(command).toBeInstanceOf(PresetSingleRegisterCommand)
       net.__socket.on('write', (data: any) => {
         expect(data).toEqual(validResponse)
@@ -252,16 +478,45 @@ describe('Server command tests', () => {
     net.__socket.emit('data', validRequest)
   })
 
-  it('should emit a ForceMultipleCoilsCommand and write a response', (done) => {
-    const validRequestBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x05, 0x0F, 0x01, 0x10, 0x00, 0x0A, 0x02, 0xCD, 0x01]
+  it('should emit a ForceMultipleCoilsCommand and write a response', done => {
+    const validRequestBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x08,
+      0x05,
+      0x0f,
+      0x01,
+      0x10,
+      0x00,
+      0x0a,
+      0x02,
+      0xcd,
+      0x01
+    ]
 
     const coilValues = [true, false, true, true, false, false, true, true, true, false]
 
-    const validResponseBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x0F, 0x01, 0x10, 0x00, 0x0A]
+    const validResponseBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x0f,
+      0x01,
+      0x10,
+      0x00,
+      0x0a
+    ]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onForceMultipleCoils.on((command) => {
+    server.onForceMultipleCoils.on(command => {
       expect(command).toBeInstanceOf(ForceMultipleCoilsCommand)
       expect(command.coilStatuses).toEqual(coilValues)
       net.__socket.on('write', (data: any) => {
@@ -274,11 +529,27 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(validRequestBytes))
   })
 
-  it('should emit an error when invalid ForceMultipleCoilsCommand is sent', (done) => {
-    const coilFailBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x05, 0x0F, 0x01, 0x10, 0x00, 0x0A, 0x03, 0xCD, 0x01]
+  it('should emit an error when invalid ForceMultipleCoilsCommand is sent', done => {
+    const coilFailBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x08,
+      0x05,
+      0x0f,
+      0x01,
+      0x10,
+      0x00,
+      0x0a,
+      0x03,
+      0xcd,
+      0x01
+    ]
     const server = new ModbusTcpServer().listen(502)
 
-    server.onCommandError.on((e) => {
+    server.onCommandError.on(e => {
       expect(e).toBeInstanceOf(ModbusCommandError)
       expect(e.message).toEqual('FORCE_MULTIPLE_COILS - Invalid coil status command received')
       expect(e.requestBytes).toEqual(Buffer.from(coilFailBytes))
@@ -288,16 +559,47 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(coilFailBytes))
   })
 
-  it('should emit a PresetMultipleRegistersCommand and write a response', (done) => {
-    const validRequestBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x0B, 0x05, 0x10, 0x01, 0x10, 0x00, 0x02, 0x04, 0x00, 0x0A, 0x01, 0x02]
+  it('should emit a PresetMultipleRegistersCommand and write a response', done => {
+    const validRequestBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x0b,
+      0x05,
+      0x10,
+      0x01,
+      0x10,
+      0x00,
+      0x02,
+      0x04,
+      0x00,
+      0x0a,
+      0x01,
+      0x02
+    ]
 
-    const registerValues = [0x000A, 0x0102]
+    const registerValues = [0x000a, 0x0102]
 
-    const validResponseBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x05, 0x10, 0x01, 0x10, 0x00, 0x02]
+    const validResponseBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x06,
+      0x05,
+      0x10,
+      0x01,
+      0x10,
+      0x00,
+      0x02
+    ]
 
     const server = new ModbusTcpServer().listen(502)
 
-    server.onPresetMultipleRegisters.on((command) => {
+    server.onPresetMultipleRegisters.on(command => {
       expect(command).toBeInstanceOf(PresetMultipleRegistersCommand)
       expect(command.registerValues).toEqual(registerValues)
       net.__socket.on('write', (data: any) => {
@@ -310,11 +612,29 @@ describe('Server command tests', () => {
     net.__socket.emit('data', Buffer.from(validRequestBytes))
   })
 
-  it('should emit an error when invalid PresetMultipleRegistersCommand is sent', (done) => {
-    const registerFailBytes = [0x00, 0x01, 0x00, 0x00, 0x00, 0x0B, 0x05, 0x10, 0x01, 0x10, 0x00, 0x02, 0x05, 0x00, 0x0A, 0x01, 0x02]
+  it('should emit an error when invalid PresetMultipleRegistersCommand is sent', done => {
+    const registerFailBytes = [
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x00,
+      0x0b,
+      0x05,
+      0x10,
+      0x01,
+      0x10,
+      0x00,
+      0x02,
+      0x05,
+      0x00,
+      0x0a,
+      0x01,
+      0x02
+    ]
     const server = new ModbusTcpServer().listen(502)
 
-    server.onCommandError.on((e) => {
+    server.onCommandError.on(e => {
       expect(e).toBeInstanceOf(ModbusCommandError)
       expect(e.message).toEqual('PRESET_MULTIPLE_REGISTERS - Invalid register command received')
       expect(e.requestBytes).toEqual(Buffer.from(registerFailBytes))
@@ -325,5 +645,4 @@ describe('Server command tests', () => {
   })
 
   // TODO: rewrite test names and test close and listen methods
-
 })
